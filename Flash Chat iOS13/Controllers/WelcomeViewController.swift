881e2170574a7eb8ffcd92cae7de4cdf1cf30c42
Jihun Park <jxxparker@gmail.com>
2020-11-01 23:01:37 -0600

    updates

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -1,25 +1,15 @@
-
-
 import UIKit
+import CLTypingLabel
 
 class WelcomeViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: CLTypingLabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        titleLabel.text = ""
-        var charIndex = 0
-        let titleText = "⚡️FlashChat"
-        for letter in titleText {
-            print(charIndex)
-            print(letter)
-            Timer.scheduledTimer(withTimeInterval: 0.1 * Double(charIndex), repeats: false) { (timer) in
-                self.titleLabel.text?.append(letter)
-            }
-            charIndex += 1
-        }
+        titleLabel.text = "⚡️FlashChat"
+        
     }
     
 
